c3ea81a9ea990797d5804b38ee22bed36df389d9
Luciano Oliveira <luciano101@msn.com>
2017-09-22 11:06:11 -0700

    loop through volunteers table and grab volunteer's name

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,6 +6,16 @@ class Volunteer
     #@id = attributes.fetch(:id)
   end
 
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = task.fetch("name")
+      volunteers.push(Volunteer.new({:name => name}))
+    end
+    volunteers
+  end
+
   def ==(another_volunteer)
     self.name().==(another_volunteer.name())
   end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -34,12 +34,13 @@ describe Volunteer do
     end
   end
 
-  xcontext '.all' do
+  context '.all' do
     it 'is empty to start' do
       expect(Volunteer.all).to eq []
     end
+  
 
-    it 'returns all volunteers' do
+  xit 'returns all volunteers' do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
