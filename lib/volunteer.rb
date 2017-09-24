be848c293773e7818261c91aeb24bcbfd60d855b
Luciano Oliveira <luciano101@msn.com>
2017-09-24 12:02:57 -0700

    find a return a volunteer by it's id

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,9 +1,10 @@
 class Volunteer
-  attr_reader(:name, :project_id)
+  attr_reader(:name, :project_id, :id)
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
   end
 
   def self.all
@@ -12,13 +13,25 @@ class Volunteer
     returned_volunteers.each() do |volunteer|
       name = volunteer.fetch("name")
       project_id = volunteer.fetch("project_id").to_i()
-      volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+      id = volunteer.fetch("id").to_i()
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
     volunteers
   end
 
   def save
-    DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id});")
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def self.find(id)
+    found_volunteer = nil
+    Volunteer.all().each() do |volunteer|
+      if volunteer.id().==(id)
+        found_volunteer = volunteer
+      end
+    end
+    found_volunteer
   end
 
   def ==(another_volunteer)
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -3,33 +3,33 @@ require('spec_helper')
 describe(Volunteer) do
   describe('#name') do
     it 'returns the name of the volunteer' do
-      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1})
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       expect(test_volunteer.name).to(eq('Jane'))
     end
   end
 
   describe('#project_id') do
     it 'returns the project_id of the volunteer' do
-      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1})
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       expect(test_volunteer.project_id).to(eq(1))
     end
   end
 
   describe '#==' do
     it 'checks for equality based on the name of a volunteer' do
-      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1})
-      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1})
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       expect(volunteer1 == volunteer2).to eq true
     end
   end
 
-  context '.all' do
+  context('.all') do
     it 'is empty to start' do
       expect(Volunteer.all).to(eq([]))
     end
 
 
-  it 'returns all volunteers' do
+  it('returns all volunteers') do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
       volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
@@ -40,13 +40,13 @@ describe(Volunteer) do
 
   describe('#save') do
     it 'adds a volunteer to the database' do
-      volunteer1 = Volunteer.new({:name => 'John', :project_id => 1})
+      volunteer1 = Volunteer.new({:name => 'John', :project_id => 1, :id => nil})
       volunteer1.save
       expect(Volunteer.all).to(eq([volunteer1]))
     end
   end
 
-  xdescribe '.find' do
+  describe '.find' do
     it 'returns a volunteer by id' do
       volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
       volunteer1.save
